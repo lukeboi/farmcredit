eec8d4d702993ef2ab58d34de9526f9cdc31dc3a
lukeboi <lukefarritor@gmail.com>
2023-05-03 02:08:02 -0500

    Working-ish login page

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
-from flask import Flask, g
+from flask import Flask, g, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import sqlite3
 
 # create flask app
 app = Flask(__name__)
+app.secret_key = 'verysecretkey'
 
 # db setup
 DATABASE = 'data.db'
+
+# initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# User class for Flask-Login
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+        
+# User loader function for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+def ensure_users_table_exists():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Users (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Username TEXT UNIQUE NOT NULL,
+            Password TEXT NOT NULL
+        )
+    """)
+    
+    cur.execute("INSERT OR IGNORE INTO Users (Username, Password) VALUES (?, ?)", ("lukefarritor", "password"))
+
+    db.commit()
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    ensure_users_table_exists()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Verify user credentials
+        cur = get_db().cursor()
+        command = "SELECT * FROM Users WHERE Username=? AND Password=?"
+        res = cur.execute(command, (username, password))
+
+        if res.fetchone() is not None:
+            user = User(username)
+            login_user(user)
+            flash('Logged in successfully.')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password.')
+
+    return render_template('login.html')
 
 # db helper functions
 def get_db():
@@ -13,6 +68,29 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully.')
+    return redirect(url_for('login'))
+
+def init_db():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Users (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Username TEXT UNIQUE NOT NULL,
+            Password TEXT NOT NULL
+        )
+    """)
+    
+    # Insert sample user
+
+    db.commit()
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -34,8 +112,11 @@ def api(city):
 
 # return static html at index route
 @app.route('/')
+@login_required
 def index():
     return app.send_static_file("index.html")
 
 if __name__ == "__main__":
+    # with app.app_context():
+    #     init_db()
     app.run(debug=True)
